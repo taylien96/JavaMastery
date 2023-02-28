89e97fcaf73f3e2e5fe625982deb606d667a9d44
Taylor Alan <tay10alan60@gmail.com>
2023-02-28 13:35:22 -0800

    variables

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
     int myNumber;
+    short myShort = 847;
     myNumber = 12;
     System.out.println(myNumber);
+    System.out.println(myShort);
+
     }
 }
