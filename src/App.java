f55438f8ffd187c1925459e72e4cc0cbc0aae2a2
Taylor Alan <tay10alan60@gmail.com>
2023-02-28 13:41:29 -0800

    learning different variable types in java

--- src/App.java
+++ src/App.java
@@ -1,10 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
     int myNumber;
-    short myShort = 847;
+    //double allows for percise int decimal, floats are small versions that are automatically doubles unless f at the end
+    float mydub = 5.24f;
     myNumber = 12;
+    char mychar = 'y';
+    //booleans are true or false as per usual
+    
+
+    System.out.println(mychar);
+
     System.out.println(myNumber);
-    System.out.println(myShort);
+    System.out.println(mydub);
 
     }
 }
