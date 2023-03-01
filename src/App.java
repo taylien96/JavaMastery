6561c209c2c775da9f55df9db6dc0eeeb3ffbe2d
Taylor Alan <tay10alan60@gmail.com>
2023-03-01 14:33:36 -0800

    welded

--- src/App.java
+++ src/App.java
@@ -1,74 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    int myNumber;
-    //double allows for percise int decimal, floats are small versions that are automatically doubles unless f at the end
-    float mydub = 5.24f;
-    myNumber = 12;
-    char mychar = 'y';
-    //booleans are true or false as per usual
-
-
-    System.out.println(mychar);
-
-    System.out.println(myNumber);
-    System.out.println(mydub);
-    class Bicycle {
-
-        int cadence = 0;
-        int speed = 0;
-        int gear = 1;
-    
-        void changeCadence(int newValue) {
-             cadence = newValue;
-        }
-    
-        void changeGear(int newValue) {
-             gear = newValue;
-        }
-    
-        void speedUp(int increment) {
-             speed = speed + increment;   
-        }
-    
-        void applyBrakes(int decrement) {
-             speed = speed - decrement;
-        }
-    
-        void printStates() {
-             System.out.println("cadence:" +
-                 cadence + " speed:" + 
-                 speed + " gear:" + gear);
-        }
-    }
-    
-    class MountainBike extends Bicycle {
-        void tireCheck(){
-            System.out.println("freshtires");
-        }
-        // new fields and methods defining 
-        // a mountain bike would go here
-    
-    }
-    //no hoisting for classes or objects
-    MountainBike dude = new MountainBike();
-     // Create two different 
-        // Bicycle objects
-        Bicycle bike1 = new Bicycle();
-        Bicycle bike2 = new Bicycle();
-
-        // Invoke methods on 
-        // those objects
-        bike1.changeCadence(50);
-        bike1.speedUp(10);
-        bike1.changeGear(2);
-        bike1.printStates();
-
-        bike2.changeCadence(50);
-        bike2.speedUp(10);
-        bike2.changeGear(2);
-        bike2.changeCadence(40);
-        bike2.speedUp(10);
-        bike2.changeGear(3);
-        bike2.printStates();
+        int myInt = 7;
+        String text = "Hello";
+        System.out.println(text);
     }
 }
