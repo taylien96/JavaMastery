9f0dde41a45c79eb05aac116f89550148f494b6c
Taylor Alan <tay10alan60@gmail.com>
2023-03-01 14:42:43 -0800

    while loops

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int myInt = 7;
-        String text = "Hello";
-        System.out.println(text);
+        int myInt = 0;
+        while(myInt < 10){
+            System.out.println(myInt);
+            myInt++;
+            myInt = myInt + 1;
+        }
+        
     }
 }
