2d74575033e1c257b3e5571e76bcab8af56a1e1b
Taylor Alan <tay10alan60@gmail.com>
2023-03-02 15:40:00 -0800

    while , for, and if statements the exact same as javascript lol

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        for(int i=0;i < 5; i++){
-            System.out.printf("The value of i is: %d\n", i);
-
-        }
+        boolean cond = 5 == 5;
+        System.out.println(cond);
     }
 }
