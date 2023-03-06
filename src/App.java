1263bb79bad2818dfc166083b55957153bf53e65
Taylor Alan <tay10alan60@gmail.com>
2023-03-06 15:57:06 -0800

    dowhile

--- src/App.java
+++ src/App.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int value = 0;
+        do{
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
 
-        
-        Scanner input = new Scanner(System.in);
-        System.out.println("enter line of text");
-        int line = input.nextInt();
-        System.out.println("you entered: " + line);
+        }while(value != 5);
+
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("enter line of text");
+        // int line = input.nextInt();
+        // System.out.println("you entered: " + line);
 
     }
 }
