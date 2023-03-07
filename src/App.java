2dd258892723ba0654f489c7a4acc586647882cc
Taylor Alan <tay10alan60@gmail.com>
2023-03-07 13:59:39 -0800

    classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
-
+class Person {
+    String name;
+    int age;
+    void speak(){
+        System.out.println("Hello! I'm " + name);
+    }
+};
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,5 +13,9 @@ public class App {
         for(String fruit: fruits){
             System.out.println(fruit);
         }
+        Person person1 = new Person();
+        person1.name = "joey";
+        person1.age = 5;
+        person1.speak();
     }
 }
