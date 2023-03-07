d03a49d1bb9fdeb00124341f925574d4d79bfb3e
Taylor Alan <tay10alan60@gmail.com>
2023-03-06 16:07:10 -0800

    array pt 1

--- src/App.java
+++ src/App.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        int value = 0;
-        do{
-            System.out.println("Enter a number: ");
-            value = scanner.nextInt();
+        int[] values;
+        values = new int[3];
+        System.out.println(values[0]);
 
-        }while(value != 5);
-
+        values[0] = 10;
+        values[1] = 20;
+        values[2] = 30;
+        
         // Scanner input = new Scanner(System.in);
         // System.out.println("enter line of text");
         // int line = input.nextInt();
         // System.out.println("you entered: " + line);
-
+        for(int i=0; i < values.length; i++){
+            System.out.println(values[i]);
+        }
+        int[] numbers = {5, 6, 7};
+        for(int i=0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
