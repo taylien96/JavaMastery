6caac9f7af3a91c8990e0a4f4f826aaed0f3dd7a
Taylor Alan <tay10alan60@gmail.com>
2023-03-06 16:09:48 -0800

    fruit: fruits

--- src/App.java
+++ src/App.java
@@ -3,24 +3,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] values;
-        values = new int[3];
-        System.out.println(values[0]);
-
-        values[0] = 10;
-        values[1] = 20;
-        values[2] = 30;
-        
-        // Scanner input = new Scanner(System.in);
-        // System.out.println("enter line of text");
-        // int line = input.nextInt();
-        // System.out.println("you entered: " + line);
-        for(int i=0; i < values.length; i++){
-            System.out.println(values[i]);
-        }
-        int[] numbers = {5, 6, 7};
-        for(int i=0; i<numbers.length; i++){
-            System.out.println(numbers[i]);
+        String[] fruits = { "apple", "banana", "pear", "kiwi"};
+        for(String fruit: fruits){
+            System.out.println(fruit);
         }
     }
 }
