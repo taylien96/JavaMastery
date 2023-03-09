c7da9bc027aeb2548afaf1c10d8007aef6411894
Taylor Alan <tay10alan60@gmail.com>
2023-03-09 15:39:13 -0800

    makin id's

--- src/App.java
+++ src/App.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 class Machine {
     public String name;
     public final static String description = "I am computer";
+    public static int count = 0;
+    public int id;
     public Machine(String name) {
         System.out.println("New machine made");
         this.name = name;
-        
+        count++;
+        this.id = count;
     }
     public void showName(){
         System.out.println(name);
@@ -23,11 +26,12 @@ public class App {
             System.out.println(fruit);
         };
         Machine machine = new Machine("talos");
+        System.out.println(Machine.count);
         Machine machine2 = new Machine("brat");
         
-        System.out.println(Machine.description);
-        System.out.println(machine.description);
-        System.out.println(machine2.name);
+        System.out.println(Machine.count);
+        System.out.println(machine.id);
+        System.out.println(machine2.id);
         System.out.println(Math.PI);
         
     }
