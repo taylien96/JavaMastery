8ce81461bbd9e0dfca133a27433b3694a75855d4
Taylor Alan <tay10alan60@gmail.com>
2023-03-09 14:47:35 -0800

    diving into static and final

--- src/App.java
+++ src/App.java
@@ -1,23 +1,8 @@
 import java.util.Scanner;
-class Machine {
-    public String name;
-    public int code;
-    public Machine(String name, int code) {
-        System.out.println("New machine made");
-        this.name = name;
-        this.code = code;
-    }
 
-};
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String[] fruits = { "apple", "banana", "pear", "kiwi"};
-        for(String fruit: fruits){
-            System.out.println(fruit);
-        }
-        Machine machine = new Machine("talos", 4);
-        System.out.println(machine.name);
         
         
     }
